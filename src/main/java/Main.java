6efcf2aec6f8c9a4816dd816f7567ea93cf9c3b1
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2021-04-16 00:43:42 +0200

    Posortowanie tablicy decyzyjnej i odleglosci

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Double[][] war;
-        String[] dec;
+        Double[][] war = new Double[0][];
+        Double[] testObject;
+        Double[] distance;
+        String[] dec = new String[0];
         int warCols = 0;
         int rows = 0;
         int decIndex = 0;
@@ -36,19 +38,48 @@ public class Main {
 
             for(int i = 0; i < array.size(); i++){
                 for(int x = 0; x < array.get(i).length ; x++){
-                    for(int y = 0; y < array.get(i)[x].length(); y++){
-                        if(x != decIndex){
-                            war[i][x] = Double.parseDouble(array.get(i)[x]);
-                        }else{
-                            dec[i] = array.get(i)[x];
-                        }
+                    if(x != decIndex){
+                        war[i][x] = Double.parseDouble(array.get(i)[x]);
+                    }else{
+                        dec[i] = array.get(i)[x];
                     }
                 }
             }
 
-            System.out.println(dec[0]);
         } catch (IOException | CsvException e) {
             e.printStackTrace();
         }
+
+        testObject = new Double[warCols];
+        for(int i = 0; i < war[0].length; i++){
+            testObject[i] = war[0][i];
+        }
+
+        distance = new Double[rows];
+        for(int i = 0; i < war.length; i++){
+            distance[i] = Math.sqrt(Math.pow((war[i][0] - testObject[0]),2) + Math.pow((war[i][1] - testObject[1]),2) + Math.pow((war[i][2] - testObject[2]),2) + Math.pow((war[i][3] - testObject[3]),2));
+        }
+
+        //sortowanie
+        int min;
+        double tempD;
+        String tempS;
+        for(int j = 0; j < distance.length - 1; j++) {
+            min = j;
+            for(int i = j + 1; i < distance.length; i++) {
+                if (distance[i] < distance[min]) {
+                    min = i;
+                    tempD = distance[min];
+                    distance[min] = distance[j];
+                    distance[j] = tempD;
+                    tempS = dec[min];
+                    dec[min] = dec[j];
+                    dec[j] = tempS;
+                }
+            }
+        }
+
+        System.out.println(dec[0] + " " + distance[0]);
+        System.out.println(dec[149] + " " + distance[149]);
     }
 }
